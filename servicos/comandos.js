07aa5b4fc85ba36169ecba9d2fe2e10b2556b461
HiagoGGA <hiagogabriel1132@gmail.com>
2025-01-31 10:47:00 -0400

    atualizar

--- servicos/comandos.js
+++ servicos/comandos.js
@@ -2,36 +2,39 @@ import pool from "./conexao.js";
 
 async function apresentartudo() {
     const conexao = await pool.getConnection()
-    const sqlQuey = "SELECT * FROM campeonatos"
-    let resposta_campeonatos = await conexao.query(sqlQuey);
+    const query = "SELECT * FROM campeonatos"
+    let resposta_campeonatos = await conexao.query(query);
     let resposta = resposta_campeonatos[0]
     conexao.release()
     return resposta
 }
 
+async function executarQuery(conexao, query) {
+    let resposta_campeonatos = await conexao.query(query);
+    let resposta = resposta_campeonatos[0]
+    return resposta
+}
+
 async function apresentarElementoId(id) {
     const conexao = await pool.getConnection()
-    const sqlQuey = `SELECT id, campeao, vice, ano FROM campeonatos WHERE id = ${id}`
-    let resposta_campeonatos = await conexao.query(sqlQuey);
-    let resposta = resposta_campeonatos[0]
+    const query = `SELECT id, campeao, vice, ano FROM campeonatos WHERE id = ${id}`
+    let resposta = executarQuery(conexao, query)
     conexao.release()
     return resposta
 }
 
 async function apresentarElementoAno(ano) {
     const conexao = await pool.getConnection()
-    const sqlQuey = `SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = ${ano}`
-    let resposta_campeonatos = await conexao.query(sqlQuey);
-    let resposta = resposta_campeonatos[0]
+    const query = `SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = ${ano}`
+    let resposta = executarQuery(conexao, query)
     conexao.release()
     return resposta
 }
 
 async function apresentarElementoCampeao(time) {
     const conexao = await pool.getConnection()
-    const sqlQuey = `SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao = "${time}"`
-    let resposta_campeonatos = await conexao.query(sqlQuey);
-    let resposta = resposta_campeonatos[0]
+    const query = `SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao = "${time}"`
+    let resposta = executarQuery(conexao, query)
     conexao.release()
     return resposta
 }
