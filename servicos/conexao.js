a748db276d8471ceb51b29401433b43f81eafad4
HiagoGGA <hiagogabriel1132@gmail.com>
2025-02-06 10:16:15 -0400

    atualizar

--- servicos/conexao.js
+++ servicos/conexao.js
@@ -1,14 +1,4 @@
 import mysql from "mysql2/promise";
-/*let conn;
-
-conn = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    passaword: 'Suporte99',
-    database: 'libertadores',
-})
-
-const conexao = await conn.connect()*/
 
 const pool = mysql.createPool({
     host: 'localhost',
