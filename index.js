94ae5353d58d13abfdeeb62293f9b65b693c6aeb
HiagoGGA <hiagogabriel1132@gmail.com>
2025-02-06 11:29:51 -0400

    atualização

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from 'express';
-import { apresentartudo, apresentarElementoId, apresentarElementoAno, apresentarElementoCampeao } from './servicos/comandos.js';
+import { apresentartudo, apresentarElementoId, apresentarElementoAno, apresentarElementoCampeao } from './servicos/comandosMostrar.js';
+import { cadastarCampeonatos } from "./servicos/comandosCadastrarCampeonatos.js"
 import pool from './servicos/conexao.js';
 const app = express();
+
+app.use(express.json())
+
+app.post('/campeonatos', async (req, res) => {
+    const campeao = req.body.campeao
+    const vice = req.body.vice
+    const ano = req.body.ano
+    await cadastarCampeonatos(campeao, vice, ano)
+    res.status(204).send({"Mensagem": "Cadastro efetivo com sucesso!"});
+})
+
 
 app.get('/campeonatos', async (req, res) => {
     const ano = req.query.ano;
@@ -9,27 +21,27 @@ app.get('/campeonatos', async (req, res) => {
     let resposta;
     if (typeof ano === 'undefined' && typeof time === 'undefined') {
         resposta = await apresentartudo()
-    } else if (typeof ano !== 'undefined' ) {
+    } else if (typeof ano !== 'undefined') {
         resposta = await apresentarElementoAno(parseInt(ano))
-    } else if (typeof time !== 'undefined' ) {
+    } else if (typeof time !== 'undefined') {
         resposta = await apresentarElementoCampeao(time)
     }
     if (resposta.length > 0) {
         res.json(resposta)
     } else {
-        res.status(404).json({mensagem: "Nenhum campeonato encontrado"})
+        res.status(404).json({ mensagem: "Nenhum campeonato encontrado" })
     }
 })
 
 app.get('/campeonatos/:id', async (req, res) => {
-    const id = parseInt(req.params.id) 
+    const id = parseInt(req.params.id)
     const resposta = await apresentarElementoId(id)
     if (resposta.length > 0) {
         res.json(resposta)
     } else {
-        res.status(404).json({mensagem: "Nenhum campeonato encontrado"})
+        res.status(404).json({ mensagem: "Nenhum campeonato encontrado" })
     }
-    
+
 })
 
 app.listen(9000, async () => {
